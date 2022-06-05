e0fd6c322de3712153d728bf7a9f9b1ba231c78b
johanesharyoseno <johanesharyoseno@yahoo.com>
2022-06-05 21:08:02 +0700

    tambahan dari pertemuan 3 juni 2022

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 const express = require ('express')
+const path = require ('path')
 const router = require ('./router')
 const app = express ()
 const port = 3000
 const logger = ( req, res, next) => {console.log (`${req.method} ${req.url}`) 
 next()}
+const foodList = require ('./food-list.json')
 app.set ('view engine', 'ejs')
 app.use (logger)
 app.use (express.json())
 app.use(express.urlencoded({extended: false}))
 app.use (router)
+app.use(express.static('src_image'))
 
+app.get('/api/v1/foods', (req,res)=>{res.status(200).json(foodList)})
+
+app.get('/api/v1/foods/:id', (req,res) => {
+    const food = foodList.find(food=>food.id==req.params.id)
+    if (food) {
+        return res.status(200).json(food)
+    }
+    return res.status(404).json({error: "makanan tidak ditemukan"})
+})
+
+app.post ('/api/v1/foods', (req,res)=>{
+    const {name, tipe, harga} = req.body
+    const id = foodList.length +1
+    const food = {
+        id,
+        name,
+        tipe,
+        harga
+    }
+    foodList.push(food)
+    res.status(201).json(food)
+})
+
+app.put ('/api/v1/foods/:id', (req, res)=> {
+    const {name, tipe, harga} = req.body
+    const id = req.params.id
+    let food = foodList.find(food=> food.id ==req.params.id)
+    food.name = name
+    food.tipe = tipe
+    food.harga = harga
+
+    foodList.map(index=> index.id == food.id ? food : index )
+    res.status(200).json (food)
+} )
+
+app.get ('/api/v1/hitungTotal', (req,res)=>{
+    let angka1= req.query.angka1 || 0
+    let angka2= req.query.angka2 || 0
+    let total= parseInt(angka1) + parseInt(angka2)
+
+    return res.status(200).json ({total:total})
+} )
+
+app.get ('/binar.png', (req, res)=> res.sendFile(path.join(__dirname,'binar.png')))
 app.get ('/', (req, res) => res.send ('Hello World!'))
 app.get ('/greeting', (req, res) => {res.render ('index')})
 app.get ('/greet', (req, res) => {const name = req.query.name || 'nama'
@@ -19,6 +66,7 @@ app.get ('/bingle', (req, res) => res.send ('Selamat datang di Bingle'))
 app.get('/register',(req,res)=>{
     res.render('register')
 })
+
 app.post ('/register', (req, res)=>{
     const { email, password} = req.body
     res.json ([email, password])
@@ -57,6 +105,9 @@ app.get ('/users', (req, res)=> res.json([
     },
     ])
 )
+
+
+
 /*app.get ('/iniError', (req,res)=>{
     iniError; //ini penyebab error
 });
